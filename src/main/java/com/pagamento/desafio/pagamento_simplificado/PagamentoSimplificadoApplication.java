3f078ca1378c2d2a743ace31d7e5af84654bfe89
Gustavo Borges <gustavo.borges.guimaraes100@gmai.com>
2024-08-17 10:38:04 -0300

    feat: project structure

---
+++ src/main/java/com/pagamento/desafio/pagamento_simplificado/PagamentoSimplificadoApplication.java
@@ -0,0 +1,13 @@
+package com.pagamento.desafio.pagamento_simplificado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagamentoSimplificadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagamentoSimplificadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pagamento/desafio/pagamento_simplificado/PagamentoSimplificadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pagamento.desafio.pagamento_simplificado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagamentoSimplificadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
