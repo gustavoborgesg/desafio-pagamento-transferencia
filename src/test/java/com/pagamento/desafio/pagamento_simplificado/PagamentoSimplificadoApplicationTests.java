2b4f571010061dbf1313330ba044e95ddc329449
Gustavo Borges <gustavo.borges.guimaraes100@gmail.com>
2024-09-07 22:54:34 -0300

    fix: removed test contextLoad

--- src/test/java/com/pagamento/desafio/pagamento_simplificado/PagamentoSimplificadoApplicationTests.java
+++ src/test/java/com/pagamento/desafio/pagamento_simplificado/PagamentoSimplificadoApplicationTests.java
@@ -1,13 +1,8 @@
 package com.pagamento.desafio.pagamento_simplificado;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PagamentoSimplificadoApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
